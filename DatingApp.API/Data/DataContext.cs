2ce45cfc823e82302702b0cd9c7340b6699f31e6
Mimina <yasmin.hamdy@gmail.com>
2020-08-19 16:53:44 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    // let our DataContext class derive from Entity Framework DbContext 
+    public class DataContext : DbContext
+    {
+         public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+         public DbSet<Value> Values { get; set; }
+    
+    }
+}
